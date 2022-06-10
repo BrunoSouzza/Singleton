60c9845001e60e721ae3de1feed4f810ffc44839
BrunoSouzza <souza_0010@hotmail.com>
2022-06-10 13:22:54 -0300

    Implementing the singleton design pattern

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> _lazySingleton = new Lazy<Singleton>((() => new Singleton()));
+        
+        private Singleton()
+        {
+
+        }
+
+
+        public static Singleton Instance
+        {
+            get { return _lazySingleton.Value; }
+        }
+    }
+}
